b4b5cbe10e3e208825bc25a181cb28f27736473e
Endre Makra-Stenkløv <69721898+EmsCodes@users.noreply.github.com>
2022-06-12 00:41:13 +0200

    styling

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const elementOne = document.querySelector("#element-one");
+const elementTwo = document.querySelector("#element-two");
+const elementThree = document.querySelector("#element-three");
